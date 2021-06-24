7c0ccbf04b5c2a914f750fb1109e3d2531288658
Shanmugarajeshwaran R <tweets.shan@gmail.com>
2021-06-24 08:11:07 +0530

    Initial Commit

---
+++ NCAlertView-Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NCAlertView-Example
+//
+//  Created by ADMIN on 24/06/21.
+//  Copyright © 2021 Success Resource Pte Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
